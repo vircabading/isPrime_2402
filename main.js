4c730bb515a6e39f76e09b6c34653b2c300d60fb
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2024-02-17 12:58:37 -0600

    implement finding a prime number

--- main.js
+++ main.js
@@ -3,11 +3,33 @@
 ///////////////////////////////////////////
 
 let isPrime = (num) => {
-    console.log("*** isPrime function: " + num + "\n");
+    // console.log("*** isPrime function: " + num + "\n");
+    for (let i=2; i<num/2; i++) {
+        // console.log(i);
+        if ( num % i == 0 ) {
+            return false;
+        }
+    }
     return true;
 }
 
 console.log("////////////////////////////////////");
 console.log("Is Prime Application\n");
 
-isPrime(11);
+console.log("Is 11 a prime number?");
+console.log(isPrime(11));
+
+console.log("\nIs 6 a prime number?");
+console.log(isPrime(6));
+
+console.log("\nIs 45 a prime number?");
+console.log(isPrime(45));
+
+console.log("\nIs 47 a prime number?");
+console.log(isPrime(47));
+
+console.log("\nIs 97 a prime number?");
+console.log(isPrime(97));
+
+console.log("\nIs 99 a prime number?");
+console.log(isPrime(99));
