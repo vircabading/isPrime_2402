f9edacb75b11ee69f589360d3c1419adca19059a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2024-02-17 12:50:58 -0600

    create isPrime function

--- main.js
+++ main.js
@@ -2,5 +2,12 @@
 // Is Prime Main Application
 ///////////////////////////////////////////
 
+let isPrime = (num) => {
+    console.log("*** isPrime function: " + num + "\n");
+    return true;
+}
+
 console.log("////////////////////////////////////");
 console.log("Is Prime Application\n");
+
+isPrime(11);
